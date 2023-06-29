97d29deb3d054df6c09c2c5eec1f61aabcf5484b
Nguyen Pham <admin@madelab.dev>
2023-06-29 16:16:40 -0500

    qweqwe

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -29,15 +29,15 @@ const IndexPage = () => {
           </div>
         </SwiperSlide>
         <SwiperSlide className="slider slider_2">
-          <div className="my-auto px-10 py-20 sm:px-15 lg:px-20 mx-auto">
-            <div className="overflow-scroll max-h-[calc(100vh-10rem)] py-2">
-              <div className="text-center leading-[1.2] font-bold text-3xl lg:text-4xl 3xl:text-5xl 6xl:text-6xl 11xl:text-7xl mb-4">
+          <div className="my-auto px-10 sm:px-15 lg:px-20 3xl:px-30 mx-auto w-full">
+            <div className="overflow-scroll max-h-screen py-2 xl:mt-20">
+              <div className="text-center leading-[1.2] font-bold text-3xl lg:text-4xl 3xl:text-5xl 6xl:text-6xl 11xl:text-7xl mt-20 mb-4">
                 This is the story of how a throw-away promotional item became an indispensable form of exposure for bands and a tool of self-expression and rebellion for generations of youth.
               </div>
               <div className="text-center leading-[1.2] font-bold text-3xl lg:text-4xl 3xl:text-5xl 6xl:text-6xl 11xl:text-7xl">
                 The band tee remains economically and culturally relevant, holding a special place in people’s hearts and wardrobes. We aim to explore and discover why. 
               </div>
-              <div className="liner mt-10 lg:mt-20 text-center">
+              <div className="liner mt-10 lg:mt-20 mb-20 text-center">
                 <div className="liner__inner"></div>
                 <div className="uppercase text-main-100 py-5 font-bold text-lg lg:text-xl 3xl:text-2xl 6xl:text-3xl 11xl:text-4xl">Worn & Faded: The Untold History of the Band Tee • 1</div>
               </div>
@@ -45,7 +45,23 @@ const IndexPage = () => {
           </div>
         </SwiperSlide>
         <SwiperSlide className="slider slider_3">
-          Slide 3
+        <div className="my-auto px-10 sm:px-15 lg:px-20 3xl:px-30 mx-auto w-full">
+          <div className="overflow-scroll max-h-screen py-2 xl:mt-10">
+              <div className="my-auto w-full mx-auto mt-20">
+                <StaticImage
+                  src="../assets/tshirt.png"
+                  alt="Worn and Faded"
+                  placeholder="blurred"
+                  layout="fullWidth"
+                  className="w-full mx-auto"
+                />
+              </div>
+              <div className="liner mt-10 lg:mt-20 mb-20 text-center">
+                <div className="liner__inner"></div>
+                <div className="uppercase text-main-100 py-5 font-bold text-lg lg:text-xl 3xl:text-2xl 6xl:text-3xl 11xl:text-4xl">Worn & Faded: The Untold History of the Band Tee • 1</div>
+              </div>
+            </div>
+          </div>
         </SwiperSlide>
         <SwiperSlide className="slider slider_4">
           Slide 4
