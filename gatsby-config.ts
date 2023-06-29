eb93dd26f693d1d156312a1d297c97ed1f762f32
Nguyen Pham <admin@madelab.dev>
2023-06-29 11:13:47 -0500

    s

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -18,14 +18,14 @@ const config: GatsbyConfig = {
     "gatsby-plugin-sitemap", {
       resolve: 'gatsby-plugin-manifest',
       options: {
-        "icon": "src/images/icon.png"
+        "icon": "src/favicon/apple-icon.png"
       }
     }, 
     "gatsby-transformer-sharp", {
     resolve: 'gatsby-source-filesystem',
     options: {
       "name": "images",
-      "path": "./src/images/"
+      "path": "./src/assets/"
     },
     __key: "images"
   }]
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,11 @@ import { StaticImage } from "gatsby-plugin-image"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
+import { Swiper, SwiperSlide } from "swiper/react";
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/navigation";
+import { Navigation } from "swiper";
 
 const IndexPage = () => {
 
@@ -11,7 +16,17 @@ const IndexPage = () => {
 
     <Layout>
 
-      sdfdsfs
+<Swiper navigation={true} modules={[Navigation]} className="mySwiper">
+        <SwiperSlide>Slide 1</SwiperSlide>
+        <SwiperSlide>Slide 2</SwiperSlide>
+        <SwiperSlide>Slide 3</SwiperSlide>
+        <SwiperSlide>Slide 4</SwiperSlide>
+        <SwiperSlide>Slide 5</SwiperSlide>
+        <SwiperSlide>Slide 6</SwiperSlide>
+        <SwiperSlide>Slide 7</SwiperSlide>
+        <SwiperSlide>Slide 8</SwiperSlide>
+        <SwiperSlide>Slide 9</SwiperSlide>
+      </Swiper>
 
     </Layout>
 
